3e032ba3a08eca49f5378ffa564ecdda750cab0c
Hop <bachngochop123plus@gmail.com>
2025-12-20 02:07:31 +0700

    feature<CN:CRUDdonhang>:Hoanh thnah 20% index

---
+++ WebsiteShopQuanAo/WebsiteShopQuanAo/Areas/Admin/Controllers/DonHangController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteShopQuanAo.Models;
+
+namespace WebsiteShopQuanAo.Areas.Admin.Controllers
+{
+    public class DonHangController : Controller
+    {
+        private QL_ShopQuanAoNuEntities db = new QL_ShopQuanAoNuEntities();
+
+        // GET: Admin/DonHang
+        public ActionResult Index()
+        {
+            var dON_HANG = db.DON_HANG.Include(d => d.KHACH_HANG);
+            return View(dON_HANG.ToList());
+        }
+
+        // GET: Admin/DonHang/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DON_HANG dON_HANG = db.DON_HANG.Find(id);
+            if (dON_HANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dON_HANG);
+        }
+
+        // GET: Admin/DonHang/Create
+        public ActionResult Create()
+        {
+            ViewBag.MAKH = new SelectList(db.KHACH_HANG, "MAKH", "HOTEN");
+            return View();
+        }
+
+        // POST: Admin/DonHang/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MADH,MAKH,NGAYDAT,HINHTHUCTHANHTOAN,DIACHIGIAO,GIAMGIA,TONGSOLUONG,TONGTHANHTIEN,TRANGTHAI")] DON_HANG dON_HANG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DON_HANG.Add(dON_HANG);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MAKH = new SelectList(db.KHACH_HANG, "MAKH", "HOTEN", dON_HANG.MAKH);
+            return View(dON_HANG);
+        }
+
+        // GET: Admin/DonHang/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DON_HANG dON_HANG = db.DON_HANG.Find(id);
+            if (dON_HANG == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MAKH = new SelectList(db.KHACH_HANG, "MAKH", "HOTEN", dON_HANG.MAKH);
+            return View(dON_HANG);
+        }
+
+        // POST: Admin/DonHang/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MADH,MAKH,NGAYDAT,HINHTHUCTHANHTOAN,DIACHIGIAO,GIAMGIA,TONGSOLUONG,TONGTHANHTIEN,TRANGTHAI")] DON_HANG dON_HANG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dON_HANG).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MAKH = new SelectList(db.KHACH_HANG, "MAKH", "HOTEN", dON_HANG.MAKH);
+            return View(dON_HANG);
+        }
+
+        // GET: Admin/DonHang/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DON_HANG dON_HANG = db.DON_HANG.Find(id);
+            if (dON_HANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dON_HANG);
+        }
+
+        // POST: Admin/DonHang/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            DON_HANG dON_HANG = db.DON_HANG.Find(id);
+            db.DON_HANG.Remove(dON_HANG);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
