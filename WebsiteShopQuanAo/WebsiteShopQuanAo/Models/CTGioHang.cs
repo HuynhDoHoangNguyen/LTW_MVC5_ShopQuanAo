a71e04397f1f0ae84e71a5534b423693ea9ab9a4
HuynhDoHoangNguyen <nguyenhuynh120305@gmail.com>
2025-12-24 19:01:10 +0700

    Hoàn thành ctsp + thêm giỏ hàng

---
+++ WebsiteShopQuanAo/WebsiteShopQuanAo/Models/CTGioHang.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteShopQuanAo.Models
+{
+    public class CTGioHang
+    {
+        public string MaCTSP { get; set; }
+        public decimal Gia { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
