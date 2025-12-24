fd4c6ebb6d1029a5cb0281d0db79979233aea89e
HuynhDoHoangNguyen <nguyenhuynh120305@gmail.com>
2025-12-25 00:16:09 +0700

    80% Cart + checkout + User

--- WebsiteShopQuanAo/WebsiteShopQuanAo/Controllers/CartController.cs
+++ WebsiteShopQuanAo/WebsiteShopQuanAo/Controllers/CartController.cs
@@ -20,6 +20,7 @@ namespace WebsiteShopQuanAo.Controllers
             
             if (Session["Cart"] == null)
             {
+
                 return View(new List<CHI_TIET_SP>());
             }
             Dictionary<string, CTGioHang> gioHang = (Dictionary<string, CTGioHang>)Session["Cart"];
@@ -63,6 +64,37 @@ namespace WebsiteShopQuanAo.Controllers
             return RedirectToAction("Index", "Cart");
         }
 
+        public ActionResult RemoveFromCart(string MaCTSP)
+        {
+            Dictionary<string, CTGioHang> gioHang = new Dictionary<string, CTGioHang>();
+
+            var ctsp = db.CHI_TIET_SP.FirstOrDefault(x => x.MACTSP == MaCTSP && x.TRANGTHAI == true);
+            //Kiểm tra session
+            if (Session["Cart"] != null)
+            {
+                gioHang = (Dictionary<string, CTGioHang>)Session["Cart"];
+            }
+
+            if (gioHang.ContainsKey(MaCTSP))
+            {
+                gioHang.Remove(MaCTSP);
+            }
+           
+
+            Session["Cart"] = gioHang;
+
+            return RedirectToAction("Index", "Cart");
+        }
+
+
+        public ActionResult ClearCart()
+        {
+            Dictionary<string, CTGioHang> gioHang = new Dictionary<string, CTGioHang>();
+
+            Session["Cart"] = gioHang;
+
+            return RedirectToAction("Index", "Cart");
+        }
 
         // GET: Cart/Details/5
         public ActionResult Details(string id)
@@ -78,6 +110,93 @@ namespace WebsiteShopQuanAo.Controllers
             }
             return View(sAN_PHAM);
         }
+
+        public ActionResult Checkout()
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            var cart = (Dictionary<string, CTGioHang>)Session["Cart"];
+
+            var ctspKeys = cart.Keys.ToList();
+
+            var ctspList = db.CHI_TIET_SP.Where(x => ctspKeys.Contains(x.MACTSP)).ToList();
+
+            var vm = new CheckoutVM
+            {
+                CTSPs = ctspList,
+                TongSoLuong = cart.Sum(x => x.Value.SoLuong),
+                TongTien = cart.Sum(x => x.Value.SoLuong * x.Value.Gia),
+                HinhThucThanhToan = "COD"
+            };
+
+            return View(vm);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PlaceOrder(CheckoutVM model)
+        {
+            if (Session["Cart"] == null || Session["MAKH"] == null)
+                return RedirectToAction("Index", "Cart");
+
+            var cart = (Dictionary<string, CTGioHang>)Session["Cart"];
+            string makh = Session["MAKH"].ToString();
+
+            using (var tran = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    // tạo đơn hàng
+                    db.SP_DONHANG_CREATE(makh,model.DiaChiGiao,model.HinhThucThanhToan);
+
+                    // lấy MADH mới nhất
+                    string madh = db.DON_HANG.Where(x => x.MAKH == makh).OrderByDescending(x => x.NGAYDAT).Select(x => x.MADH).First();
+
+                    // thêm chi tiết đơn hàng
+                    foreach (var item in cart)
+                    {
+                        var ctsp = db.CHI_TIET_SP.First(x => x.MACTSP == item.Key);
+
+                        db.SP_CTDH_ADD(
+                            madh,
+                            ctsp.MASP,
+                            ctsp.MAMAU,
+                            ctsp.MASIZE,
+                            item.Value.SoLuong
+                        );
+                    }
+
+                    tran.Commit();
+                    Session["Cart"] = null;
+
+                    return RedirectToAction("Success");
+                }
+                catch
+                {
+                    tran.Rollback();
+                    ViewBag.Error = "Không đủ tồn kho hoặc lỗi hệ thống";
+                    return View("Checkout", model);
+                }
+            }
+        }
+
+        public ActionResult Success()
+        {
+            if (Session["MAKH"] == null)
+                return RedirectToAction("Index", "Home");
+
+            return View();
+        }
+
+
+
+
+
 
         // GET: Cart/Create
         public ActionResult Create()
---
+++ WebsiteShopQuanAo/WebsiteShopQuanAo/Models/CheckoutVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteShopQuanAo.Models
+{
+    public class CheckoutVM
+    {
+        // thông tin giao hàng
+        public string DiaChiGiao { get; set; }
+
+        // COD / BANK
+        public string HinhThucThanhToan { get; set; }
+
+        // hiển thị
+        public decimal TongTien { get; set; }
+        public int TongSoLuong { get; set; }
+
+        // danh sách CTSP trong giỏ
+        public List<CHI_TIET_SP> CTSPs { get; set; }
+    }
+
+}
